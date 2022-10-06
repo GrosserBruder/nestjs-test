2bfa2846a98712d00ac2c0a5fa1ef867bb5fe963
GrosserBruder <falenangellll@yandex.ru>
2022-10-06 10:02:13 +0700

    fix

--- src/dhl/entity/dhl.entity.ts
+++ src/dhl/entity/dhl.entity.ts
@@ -1,12 +1,12 @@
 import { DhlTypeEntity } from './../../dhlTypes/entity/dhl-type.entity';
 import { BaseEntity } from '../../common/base.entity';
-import { Column, Entity, ManyToOne } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
 
 @Entity()
 export class DhlEntity extends BaseEntity {
   @Column()
   name: string
-  @ManyToOne(() => DhlTypeEntity, (dhlTypeEntity) => dhlTypeEntity.dhls, {
+  @OneToMany(() => DhlTypeEntity, (dhlTypeEntity) => dhlTypeEntity.dhls, {
     cascade: false
   })
   type: DhlTypeEntity
--- src/dhlTypes/entity/dhl-type.entity.ts
+++ src/dhlTypes/entity/dhl-type.entity.ts
@@ -1,12 +1,12 @@
 import { DhlEntity } from './../../dhl/entity/dhl.entity';
 import { BaseEntity } from './../../common/base.entity';
-import { Column, Entity, OneToMany } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity()
 export class DhlTypeEntity extends BaseEntity {
   @Column()
   name: string
-  @OneToMany(() => DhlEntity, (dhlEntity) => dhlEntity.type, {
+  @ManyToOne(() => DhlEntity, (dhlEntity) => dhlEntity.type, {
     cascade: false,
   })
   dhls: Array<DhlEntity>
