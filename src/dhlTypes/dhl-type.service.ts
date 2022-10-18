f3b3dc2f851919239f0b6ddc812c687055c55a00
GrosserBruder <falenangellll@yandex.ru>
2022-10-18 11:17:07 +0700

    imports fix

--- src/dhlTypes/dhl-type.service.ts
+++ src/dhlTypes/dhl-type.service.ts
@@ -1,10 +1,8 @@
-import { DhlTypeEntity } from './entity/dhl-type.entity';
 import { Injectable } from "@nestjs/common";
-import { CreateDhlTypeDto } from "./dto/create-dhl-type.dto";
-import { UpdateDhlTypeDto } from "./dto/update-dhl-type.dto";
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { BaseService } from 'src/common/BaseService';
+import { Repository } from 'typeorm';
+import { DhlTypeEntity } from './entity/dhl-type.entity';
 
 @Injectable()
 export class DhlTypeService extends BaseService<DhlTypeEntity> {
